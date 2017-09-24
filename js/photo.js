6a06cc19b31df242be7751ac96664af2593647f1
Usman Farooqi <uhfarooqi@gmail.com>
2017-09-24 00:37:44 -0500

    Camera fully works now
    now we just gotta figure out the other 90% of the project

--- js/photo.js
+++ js/photo.js
@@ -1,5 +1,8 @@
 (function() {
-  var video = document.getElementById('video');
+  var video = document.getElementById('video'),
+    canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d'),
+    photo = document.getElementById("photo"),
     vendorUrl = window.URL || window.webkitURL;
 
   navigator.getMedia =  navigator.getUserMedia ||
@@ -17,5 +20,10 @@
     // An error occured
     // error.code
   })
+
+  document.getElementById('capture').addEventListener('click', function() {
+    context.drawImage(video, 0, 0, 1280, 720);
+    photo.setAttribute('src', canvas.toDataURL('image/png'))
+  });
 
 })();
